c1df9972f331004438b0f5fc948a64d9ec0c9137
Ágnes Kuti-Palasthy <agnesnora@gmail.com>
2024-02-27 11:40:12 +0100

    setting up css variables

--- src/App.tsx
+++ src/App.tsx
@@ -9,3 +9,14 @@ function App() {
 }
 
 export default App;
+
+//Creating reusable components for a Navbar
+//Button, menubutton, menudropdown, menuitem
+
+//Creating Navbar component
+
+// Creating an Options component
+
+//Creating a planetdetails component
+
+// creating the  planetdatabox component
