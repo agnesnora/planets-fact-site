1cf121c9efb5e4149ea613431292ebdccc094a68
Ágnes Kuti-Palasthy <agnesnora@gmail.com>
2024-03-01 13:03:00 +0100

    refactoring

---
+++ src/Components/Planets/PlanetDetailComponent.tsx
@@ -0,0 +1,70 @@
+import React, { FC, useState } from "react";
+import { PlanetData } from "../../../data";
+import { Button } from "../Button/Button";
+import { Link } from "react-router-dom";
+
+interface PlanetDetailProps {
+  planet: PlanetData | undefined;
+  view: string;
+  setView: React.Dispatch<React.SetStateAction<string>>;
+}
+
+export const PlanetDetailComponent: FC<PlanetDetailProps> = ({
+  planet,
+  view,
+  setView,
+}) => {
+  const handleOverviewClick = () => {
+    setView("planet");
+  };
+
+  const handleStructureClick = () => {
+    setView("internal");
+  };
+
+  const handleSurfaceClick = () => {
+    setView("geology");
+  };
+
+  if (!planet) {
+    console.error("Planet data is not available");
+    return null;
+  }
+
+  const planetImage: string =
+    view === "planet"
+      ? planet.images.planet
+      : view === "geology"
+      ? planet.images.geology
+      : planet.images.internal;
+
+  return (
+    <div>
+      <div className="view--container">
+        <Button onClick={handleOverviewClick}>Overview</Button>
+        <Button onClick={handleStructureClick}>Structure</Button>
+        <Button onClick={handleSurfaceClick}>Surface</Button>
+      </div>
+      <img src={planetImage} alt={`${planet.name} image`} />
+      <div className="planet--intro">
+        <h1>{planet.name.toUpperCase()}</h1>
+        <p>{planet.overview.content}</p>
+        <Link to={planet.overview.source}>Source: Wikipedia</Link>
+      </div>
+      <div>
+        <div className="fact--box">
+          <h4>Rotation time {planet.rotation}</h4>
+        </div>
+        <div className="fact--box">
+          <h4>Revolution time{planet.revolution}</h4>
+        </div>
+        <div className="fact--box">
+          <h4>Radius{planet.radius}</h4>
+        </div>
+        <div className="fact--box">
+          <h4>Temperature{planet.temperature}</h4>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/Components/Planets/PlanetsDetailPage.tsx
+++ src/Components/Planets/PlanetsDetailPage.tsx
@@ -1,8 +1,9 @@
-import { useParams, Link } from "react-router-dom";
-import { FC, useContext, useState } from "react";
+// PlanetsDetailPage.tsx
+import React, { FC, useContext, useState } from "react";
 import { AppContext } from "../../App";
+import { useParams } from "react-router-dom";
+import { PlanetDetailComponent } from "./PlanetDetailComponent";
 import { PlanetData } from "../../../data";
-import { Button } from "../Button/Button";
 
 interface RouteParams {
   name: string;
@@ -14,73 +15,22 @@ const isRouteParams = (params: any): params is RouteParams => {
 
 export const PlanetsDetailPage: FC = () => {
   const context = useContext(AppContext);
-
   const params = useParams();
   const [view, setView] = useState<string>("planet");
-  const handleOverviewClick = () => {
-    setView("planet");
-  };
-  const handleStructureClick = () => {
-    setView("internal");
-  };
-  const handleSurfaceClick = () => {
-    setView("geology");
-  };
 
-  if (!isRouteParams(params)) {
-    console.error("Invalid params:", params);
-    return null;
-  }
-  if (!context) {
-    console.error("Context is not available");
+  if (!isRouteParams(params) || !context) {
+    console.error("Invalid params or context not available:", params, context);
     return null;
   }
 
   const typedParams = params as RouteParams;
-  console.log(typedParams);
-  const planet: PlanetData | undefined = context?.planets.find(
+  const planet: PlanetData | undefined = context.planets.find(
     (planet) => planet.name === typedParams.name
   );
-  if (!planet) {
-    // Handle the case where the planet is not found
-    console.error(`Planet with name ${typedParams.name} not found`);
-    return null;
-  }
-  console.log(planet);
-  const planetImage: string =
-    view === "planet"
-      ? planet.images.planet
-      : view === "geology"
-      ? planet.images.geology
-      : planet.images.internal;
 
   return (
-    <div>
-      <div className="view--container">
-        <Button onClick={handleOverviewClick}>Overview</Button>
-        <Button onClick={handleStructureClick}>Structure</Button>
-        <Button onClick={handleSurfaceClick}>Surface</Button>
-      </div>
-      <img src={planetImage} />
-      <div className="planet--intro">
-        <h1>{planet.name.toUpperCase()}</h1>
-        <p>{planet.overview.content}</p>
-        <Link to={planet.overview.source}>Source: Wikipedia</Link>
-      </div>
-      <div>
-        <div className="fact--box">
-          <h4>Rotation time {planet.rotation}</h4>
-        </div>
-        <div className="fact--box">
-          <h4>Revolution time{planet.revolution}</h4>
-        </div>
-        <div className="fact--box">
-          <h4>Radius{planet.radius}</h4>
-        </div>
-        <div className="fact--box">
-          <h4>Temperature{planet.temperature}</h4>
-        </div>
-      </div>
-    </div>
+    <PlanetDetailComponent planet={planet} view={view} setView={setView} />
   );
 };
+
+export default PlanetsDetailPage;
